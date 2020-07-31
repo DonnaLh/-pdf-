9b34bf92f0d5e8cd0e28d97d642a44acc08360b7
donnaLH <2101899679@qq.com>
2020-07-31 09:53:55 +0800

    web-view使用

---
+++ pages/filePreview.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		<web-view :src="url"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				url: '',
+			};
+		},
+		onLoad(params){
+			let {url} = params
+			this.url = `/hybrid/html/pdf.html?url=${url}`
+		},
+		onBackPress(){}
+	}
+</script>
+
+<style>
+</style>
